cd72011d92f7c2c333f63da360629923f07f30dd
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-04-07 19:03:08 +0200

    bgi

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.innerBox}>
-        <Text style={styles.text}>Hello Andrii</Text>
-      </View>
+      <ImageBackground
+        source={require("./assets/images/fon.jpeg")}
+        style={styles.image}
+      >
+        <View style={styles.innerBox}>
+          <Text style={styles.text}>Hello Andrii</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -15,8 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   text: {
     color: "red",
@@ -28,5 +33,11 @@ const styles = StyleSheet.create({
     padding: 40,
     borderRadius: 20,
     width: 250,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
